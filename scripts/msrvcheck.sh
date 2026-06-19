825f4fb1bd901a64826660d716f945b0f7ffc663
Gergő Sályi <salyigergo94@gmail.com>
2026-06-19 12:01:59 +0200

    Increase minimum supported Rust version to 1.88, update msrv check script

--- scripts/msrvcheck.sh
+++ scripts/msrvcheck.sh
@@ -2,7 +2,8 @@
 # rustup toolchain uninstall 1.80-x86_64-unknown-linux-gnu
 # rustup toolchain install 1.82-x86_64-unknown-linux-gnu
 # rustup +1.82-x86_64-unknown-linux-gnu target add aarch64-unknown-linux-gnu x86_64-unknown-freebsd
-set -euxo pipefail
-cargo +1.87-x86_64-unknown-linux-gnu check --target=x86_64-unknown-linux-gnu --features=avif
-cargo +1.87-x86_64-unknown-linux-gnu check --target=aarch64-unknown-linux-gnu
-cargo +1.87-x86_64-unknown-linux-gnu check --target=x86_64-unknown-freebsd
+set -Ceuxo pipefail
+toolchain=1.88-x86_64-unknown-linux-gnu
+cargo +${toolchain} clippy --target=x86_64-unknown-linux-gnu --features=avif
+cargo +${toolchain} clippy --target=aarch64-unknown-linux-gnu
+cargo +${toolchain} clippy --target=x86_64-unknown-freebsd
